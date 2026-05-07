7f1cabc8a0aee01c39d521274330644fcf57b58b
Anctro <xxyao1994@163.com>
2026-05-07 19:11:10 +0800

    feat: add basePath /vocabcoach and standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/vocabcoach',
+  output: 'standalone',
 };
 
 export default nextConfig;
